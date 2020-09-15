a5e952f23b5a3296d601fe89f9f6031e4c5e6871
ramcj7742 <ramcj7742@wrdsb.ca>
2020-09-15 13:03:26 +0000

    Finished quotent/remainder problem

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Asks for two numbers then divides and shows * * quotent and remainder
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +11,37 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Asks user for two numbers on seperate lines
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+
+    //Declare variable for first number
+    int firstNum;
+
+    //Declare variable for second number
+    int secondNum;
+
+    //records users first number
+    firstNum = input.nextInt();
+
+    //records users second number
+    secondNum = input.nextInt();
+
+    //Calculates quotent
+    int quo = firstNum / secondNum;
+
+    //Calculates remainder
+    int rem = firstNum % secondNum;
+
+    //Tells user the quotent and remainder of their two numers
+    System.out.println(firstNum + "/" + secondNum + " is " + quo + "  with a remainder of " + rem + ".");
+
+
+
+
+    
     
   }
 }
